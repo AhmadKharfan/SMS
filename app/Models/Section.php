9f6620a2d2e3294e08e72afb29eada857b201042
AhmadKharfan <plamoplamo12@example.com>
2025-08-30 15:35:39 +0000

    first commit

---
+++ app/Models/Section.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+
+class Section extends Model
+{
+    use HasTranslations;
+    public $translatable = ['Name_Section'];
+    protected $fillable=['Name_Section','Grade_id','Class_id'];
+
+    protected $table = 'sections';
+    public $timestamps = true;
+
+
+    // علاقة بين الشعب والصفوف لجلب اسم الصف في جدول الشعب
+
+    public function My_classs()
+    {
+        return $this->belongsTo('App\Models\Classroom', 'Class_id');
+    }
+
+    // علاقة الشعب مع المعلمين
+    public function teachers()
+    {
+        return $this->belongsToMany('App\Models\Teacher','teacher_section');
+    }
+
+    public function Grades()
+    {
+        return $this->belongsTo('App\Models\Grade','Grade_id');
+    }
+
+}
